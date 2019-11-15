2755e7586215fd5fe13c69ff60834a26a60fe1a4
victor.arana <victor.arana@walmart.com>
2019-11-15 10:17:03 -0600

    feature, links action button to Order Activity.

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,10 +1,13 @@
 package com.hfad.bitsandpizzas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                // Code to run when the Create Order Item is clicked.
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                // Return true tell Android  you've dealt with the item being clicked
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
